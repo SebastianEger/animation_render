ac8c18004b80c14709081b11baf72db56f670f24
Sebastian Eger <sebastian.eger@tum.de>
2018-02-27 14:25:46 +0100

    Added setuptools.
    Former-commit-id: f103406677eacf61bf4280cfd5af2d9b2edd9579
    Former-commit-id: 44d0f36dd316ce991abb7bc9cdcd428ebe41b927
    Former-commit-id: 25ff485e51a489c646cf9930867f551194ef48d1

--- install_script.sh
+++ install_script.sh
@@ -4,6 +4,9 @@ sudo apt-get update
 sudo apt-get install python3.6
 sudo apt-get install python3.6-dev
 
+# Install setuptools
+wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python3.6
+
 # Install blender dependencies
 sudo ./build_environment/install_deps.sh
 
