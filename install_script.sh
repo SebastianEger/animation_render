30a229eb5f23a9b4c4899131c2d008a1edb04789
Sebastian Eger <sebastian.eger@tum.de>
2018-02-27 16:12:36 +0100

    Using pip instead of setuptools.

--- install_script.sh
+++ install_script.sh
@@ -5,10 +5,13 @@ sudo apt-get install python3.6
 sudo apt-get install python3.6-dev
 
 # Install setuptools
-wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python3.6
+sudo apt-get install python3-pip
+sudo python3.6 -m pip install Pillow
+sudo python3.6 -m pip install flickrapi
+sudo python3.6 -m pip install pyyaml
 
 # Install blender dependencies
 sudo ./build_environment/install_deps.sh
 
 # Install script dependencies
-sudo python3.6 ./scripts/setup.py install
+# sudo python3.6 ./scripts/setup.py install
